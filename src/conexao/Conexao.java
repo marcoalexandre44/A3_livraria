1f3af932b79ec38efd31094b06b5ded2ca1d57b3
AlefeCma <alefedonato777hotmail.com@gmail.com>
2022-09-14 12:00:05 -0300

    Divisão das classes

---
+++ src/conexao/Conexao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author alefe
+ */
+public class Conexao {
+   Connection cn = null;
+   
+   
+   
+   
+}
